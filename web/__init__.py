545c6409fe48ddfa93a97488aa0faf80f05128b3
SpikyShade <103762012+spikyshade@users.noreply.github.com>
2023-01-03 16:22:15 +0000

    idk

--- web/__init__.py
+++ web/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from .views import views
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRECT_KEY'] = 'jsodhsbosjdbsjsjsjsbd'
+app = Flask(__name__)
+app.config['SECRECT_KEY'] = 'jsodhsbosjdbsjsjsjsbd'
 
-    app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(views, url_prefix='/')
 
-    return app
     
-create_app()
+if __name__=='__main__':
+    app.run(host='0.0.0.0')
