8e485d7bc5364be146ee563b9bb0afa1b4c7cb96
SpikyShade <103762012+spikyshade@users.noreply.github.com>
2023-01-03 16:37:22 +0000

    reverse

--- web/__init__.py
+++ web/__init__.py
@@ -1,14 +1,10 @@
-try:
-    from flask import Flask
-    from views import views
-except Exception as e:
-    print(f'error occured: {e}')
+from flask import Flask
+from .views import views
 
-app = Flask(__name__)
-app.config['SECRECT_KEY'] = 'jsodhsbosjdbsjsjsjsbd'
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRECT_KEY'] = 'jsodhsbosjdbsjsjsjsbd'
 
-app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
 
-    
-if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    return app
