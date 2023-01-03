14e648f0fe40f3f3c11262367f972e70ea71b74d
SpikyShade <103762012+spikyshade@users.noreply.github.com>
2023-01-03 16:51:28 +0000

    idkkkk

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from web import create_app
+# import sys
+# sys.path.insert(1, '/home/runner/Deploy/web')
+# from flask import Flask
+# from views import views
 
-app = create_app()
+# app = Flask(__name__)
+# app.config['SECRECT_KEY'] = 'jsodhsbosjdbsjsjsjsbd'
+
+# app.register_blueprint(views, url_prefix='/')
+from web import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- web/__init__.py
+++ web/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from .views import views
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRECT_KEY'] = 'jsodhsbosjdbsjsjsjsbd'
-
-    app.register_blueprint(views, url_prefix='/')
-
-    return app
+app = Flask(__name__)
+app.config['SECRECT_KEY'] = 'jsodhsbosjdbsjsjsjsbd'
+app.register_blueprint(views, url_prefix='/')
